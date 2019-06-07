93638fa4f8bafecad59c04065f675b4de6b58ac0
emb2ug <47042768+emb2ug@users.noreply.github.com>
2019-06-07 16:09:35 -0400

    frontend and backend working together

--- src/app/current-weather/current-weather.component.ts
+++ src/app/current-weather/current-weather.component.ts
@@ -18,6 +18,7 @@ export class CurrentWeatherComponent implements OnInit {
   tempF = 0;
   rain = false;
   clear = false;
+  key = "";
 
   constructor(private data: DataService, private formBuilder: FormBuilder) {
     this.locationForm = this.formBuilder.group({
@@ -46,21 +47,24 @@ export class CurrentWeatherComponent implements OnInit {
     // Do useful stuff with the gathered data
     console.log(this.cityName);
 
-    this.data.getCurrentWeather(this.cityName).subscribe(data => {
-      this.rain = false;
-      this.clear = false;
-      this.result = data;
-      this.condition = data.weather[0].main;
-      this.tempK = data.main.temp;
-      this.tempF = this.tempK * (9 / 5) - 459.67;
-      this.tempF = Math.round(this.tempF);
-      console.log(this.result);
+    this.data.getKey().subscribe(response => {
+      this.key = response.key;
+      this.data.getCurrentWeather(this.cityName, this.key).subscribe(data => {
+        this.rain = false;
+        this.clear = false;
+        this.result = data;
+        this.condition = data.weather[0].main;
+        this.tempK = data.main.temp;
+        this.tempF = this.tempK * (9 / 5) - 459.67;
+        this.tempF = Math.round(this.tempF);
+        console.log(this.result);
 
-      if (this.condition == "Rain") {
-        this.rain = true;
-      } else if (this.condition == "Clear") {
-        this.clear = true;
-      }
+        if (this.condition == "Rain") {
+          this.rain = true;
+        } else if (this.condition == "Clear") {
+          this.clear = true;
+        }
+      });
     });
   }
 
